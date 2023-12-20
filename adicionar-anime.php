19ca4ca3236778d5b670bd8c5d91cd0519eec1ec
Yago Sodre <yagobk@gmail.com>
2023-12-20 08:27:37 -0300

    feat: index e página adicionar-anime

---
+++ adicionar-anime.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicionar Anime</title>
+    <link rel="stylesheet" type="text/css" href="/css/styles.css">
+</head>
+<body>
+    <h1>Adicionar Anime</h1>
+    
+    <form action="processar_adicao.php" method="post">
+        <label for="nome">Nome do Anime:</label>
+        <input type="text" id="nome" name="nome" required>
+
+        <label for="nota">Nota:</label>
+        <input type="number" id="nota" name="nota" min="0" max="10" step="0.1" required>
+
+        <label for="categoria">Categoria:</label>
+        <select id="categoria" name="categoria" required>
+            <option value="ja_assistiu">Já Assistiu</option>
+            <option value="assistindo">Assistindo</option>
+            <option value="pretende_assistir">Pretende Assistir</option>
+        </select>
+
+        <button type="submit">Adicionar</button>
+    </form>
+
+    <a href="index.php">Voltar para a Lista</a>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Controle de Animes</title>
+    <link rel="stylesheet" type="text/css" href="/css/styles.css">
+</head>
+<body>
+    <h1>Controle de Animes</h1>
+    
+    <h2>Já Assistiu</h2>
+    <ul>
+        <!-- Lista de animes já assistidos -->
+        <!-- Exemplo: <li>Anime A - Nota: 8</li> -->
+    </ul>
+
+    <h2>Assistindo</h2>
+    <ul>
+        <!-- Lista de animes em andamento -->
+    </ul>
+
+    <h2>Pretende Assistir</h2>
+    <ul>
+        <!-- Lista de animes que pretende assistir -->
+    </ul>
+
+    <a href="adicionar-anime.php">Adicionar Anime</a>
+</body>
+</html>
