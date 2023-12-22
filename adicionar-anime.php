cc25baea44eda5d906d7ae0f5a8dd0955e00a738
Yago Sodre <yagobk@gmail.com>
2023-12-21 23:55:38 -0300

    refactor: ajustes css do index

--- adicionar-anime.php
+++ adicionar-anime.php
@@ -2,30 +2,34 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Adicionar Anime</title>
     <link rel="stylesheet" type="text/css" href="/css/styles.css">
 </head>
 <body>
-    <h1>Adicionar Anime</h1>
+    <header></header>
+    <main>
+        <h1>Adicionar Anime</h1>
     
-    <form action="processar_adicao.php" method="post">
-        <label for="nome">Nome do Anime:</label>
-        <input type="text" id="nome" name="nome" required>
+        <form action="processar_adicao.php" method="post">
+            <label for="nome">Nome do Anime:</label>
+            <input type="text" id="nome" name="nome" required>
 
-        <label for="nota">Nota:</label>
-        <input type="number" id="nota" name="nota" min="0" max="10" step="0.1">
+            <label for="nota">Nota:</label>
+            <input type="number" id="nota" name="nota" min="0" max="10" step="0.1">
 
-        <label for="categoria">Categoria:</label>
-        <select id="categoria" name="categoria" required>
-            <option value="finalizado">Finalizado</option>
-            <option value="assistindo">Assistindo</option>
-            <option value="pretende_assistir">Pretende Assistir</option>
-        </select>
+            <label for="categoria">Categoria:</label>
+            <select id="categoria" name="categoria" required>
+                <option value="finalizado">Finalizado</option>
+                <option value="assistindo">Assistindo</option>
+                <option value="pretende_assistir">Pretende Assistir</option>
+            </select>
 
-        <button type="submit">Adicionar</button>
-    </form>
-
-    <a href="index.php">Voltar para a Lista</a>
+            <button type="submit">Adicionar</button>
+        </form>
+    </main>
+    <footer></footer>
+    <a href="index.html">Voltar para a Lista</a>
 </body>
 </html>
