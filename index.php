9e618bcbcddd75982cbf80fecb8495ac7b3cc369
Yago Sodre <yagobk@gmail.com>
2024-01-24 22:32:33 -0300

    refactor: bootstrap index.php

--- index.php
+++ index.php
@@ -17,23 +17,23 @@ $dadosPretendeAssistir = $animeRepositorio->listaPretendeAssistir();
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Listas de Animes</title>
-    <link rel="stylesheet" type="text/css" href="/css/styles.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 </head>
-<body>
+<body class="bg-dark text-light">
     <header></header>
     <main class="animes">
-        <section class="animes__conteudo">
-            <h1 class="animes__conteudo__titulo">Listas de Animes</h1>
+        <section class="container mt-4">
+            <h1 class="text-center mb-4">Listas de Animes</h1>
             
-            <div class="animes__conteudo__opcoes">
-                <a class="animes__conteudo__adicionar" href="adicionar-anime.php">Adicionar Anime</a>
-                <a class="animes__conteudo__adicionar" href="editar-anime.php">Editar Anime</a>
+            <div class="d-flex justify-content-left gap-4 mb-4">
+                <a class="btn btn-primary" href="adicionar-anime.php">Adicionar Anime</a>
+                <a class="btn btn-secondary" href="editar-anime.php">Editar Anime</a>
             </div>
 
-            <div class="animes__conteudo__categorias">
-                <div class="finalizado">
-                    <h2>Finalizado</h2>
-                    <ul>
+            <div class="row">
+                <div class="col">
+                    <h2 class="mb-3">Finalizado</h2>
+                    <ul class="list-group">
                         <?php foreach ($dadosFinalizado as $finalizado): ?>
                             <li>
                                 <?= $finalizado->getNome(); ?>
@@ -45,9 +45,9 @@ $dadosPretendeAssistir = $animeRepositorio->listaPretendeAssistir();
                     </ul>
                 </div>
 
-                <div class="assistindo">
-                    <h2>Assistindo</h2>
-                    <ul>
+                <div class="col">
+                    <h2 class="mb-3">Assistindo</h2>
+                    <ul class="list-group">
                         <?php foreach ($dadosAssistindo as $assistindo): ?>
                             <li>
                                 <?= $assistindo->getNome(); ?>
@@ -59,9 +59,9 @@ $dadosPretendeAssistir = $animeRepositorio->listaPretendeAssistir();
                     </ul>
                 </div>
 
-                <div class="pretende">
-                    <h2>Pretende Assistir</h2>
-                    <ul>
+                <div class="col">
+                    <h2 class="mb-3">Pretende Assistir</h2>
+                    <ul class="list-group">
                         <?php foreach ($dadosPretendeAssistir as $pretendeAssistir): ?>
                             <li>
                                 <?= $pretendeAssistir->getNome(); ?>
@@ -76,5 +76,6 @@ $dadosPretendeAssistir = $animeRepositorio->listaPretendeAssistir();
         </section>
     </main>
     <footer></footer>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
 </body>
 </html>
